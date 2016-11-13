63cca82f65fd7739a14c66edf28fb6c5ae3807e9
Josh Sherman <joshua.v.sherman@gmail.com>
2016-11-13 15:43:37 -0500

    Added ENV for Origin URL

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ if(process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === undefined){
 const app  = express();
 
 app.use(function(req, res, next){
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:9000");
+  res.setHeader("Access-Control-Allow-Origin", process.env.AllowUrl);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
