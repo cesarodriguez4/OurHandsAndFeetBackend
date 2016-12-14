27f2f86c78e6461d30898291903b12a3f85b34ea
Josh Sherman <joshua.v.sherman@gmail.com>
2016-12-14 15:32:37 -0500

    code cleanup

--- auth/google.js
+++ auth/google.js
@@ -1,21 +1,16 @@
-'use strict';
-var User = require('../model/user/user-schema.js');
-var config = require('../config');
-var request = require('request');
-var jwt = require('jwt-simple');
-var authUtils = require('./authUtils');
-
-
+const User = require('../model/user/user-schema');
+//const config = require('../config');
+const request = require('request');
+//const jwt = require('jwt-simple');
+const authUtils = require('./authUtils');
 
 exports.authenticate = function (req, res) {
-  var accessTokenUrl = 'https://accounts.google.com/o/oauth2/token';
-  var peopleApiUrl = 'https://www.googleapis.com/plus/v1/people/me/openIdConnect';
-  //console.log(req.body);
-  var params = {
+  const accessTokenUrl = 'https://accounts.google.com/o/oauth2/token';
+  const peopleApiUrl = 'https://www.googleapis.com/plus/v1/people/me/openIdConnect';
+
+  const params = {
     code: req.body.code,
     client_id: req.body.clientId,
-//TODO: if dev use local .env file
-//TODO: if hosted in heroku use GoogleClientSecret
     client_secret: process.env.GoogleClientSecret,
     redirect_uri: req.body.redirectUri,
     grant_type: 'authorization_code'
@@ -25,15 +20,14 @@ exports.authenticate = function (req, res) {
   request.post(accessTokenUrl, { json: true, form: params }, function(err, response, token) {
     //console.log("After initial access");
     //console.log(token);
-    var accessToken = token.access_token;
+    const accessToken = token.access_token;
     //console.log(accessToken);
-    var headers = { Authorization: 'Bearer ' + accessToken };
+    const headers = { Authorization: 'Bearer ' + accessToken };
 
     // Step 2. Retrieve profile information about the current user.
     request.get({ url: peopleApiUrl, headers: headers, json: true }, function(err, response, profile) {
       //console.log("Got Profile Info");
-      console.log(profile);
-      // // Step 3a. Link user accounts.
+          // // Step 3a. Link user accounts.
       // if (req.headers.authorization) {
       //   User.findOne({ google: profile.sub }, function(err, existingUser) {
       //     if (existingUser) {
@@ -68,18 +62,12 @@ exports.authenticate = function (req, res) {
             user.name = profile.name;
             user.email = profile.email;
             user.save(function(err) {
-              var token = authUtils.createJWT(user);
-               console.log("token sent");
-
-             res.send({ token: token });
+              const token = authUtils.createJWT(user);
+              console.log('token sent');
+              res.send({ token: token });
             });
           }
-          //     console.log("token sent");
-          //
-          //   res.send({ token: token });
-          });
         });
-      //}
     });
-  //});
-}
+  });
+};
