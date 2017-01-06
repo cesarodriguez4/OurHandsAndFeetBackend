4f64056dc28da9181b0c260d17f0c8dfb047d9dc
Josh Sherman <joshua.v.sherman@gmail.com>
2017-01-06 11:52:50 -0500

    added circle ci readme badge and yml

--- routes.js
+++ routes.js
@@ -2,23 +2,22 @@ const Router = require('express').Router;
 const router = new Router();
 const user  = require('./model/user/user-router');
 
-function authenticate(req, res, next){
-    if(req.headers.origin == process.env.AllowUrl){
-      next();
-    }else{
-      res.redirect("/");
-    }
+function authenticate(req, res, next) {
+  if (req.headers.origin === process.env.AllowUrl) {
+    next();
+  } else {
+    res.redirect('/');
+  }
 }
-
 
 
 router.route('/').get((req, res) => {
   res.json({ message: 'Welcome to Our Hands and Feet API!' });
 });
-router.all("/*", authenticate, function(req, res, next){
+router.all('/*', authenticate, function(req, res, next) {
   next();
 });
 router.use('/user', user);
-router.use('/auth',require('./auth'));
+router.use('/auth', require('./auth'));
 
 module.exports = router;
