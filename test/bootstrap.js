cb9ca483caf2ca15d45533722daef861a63f1f4b
Josh Sherman <joshua.v.sherman@gmail.com>
2017-01-30 16:40:03 -0500

    few more tests that pass, and moved the mockgoose out of the bootstrap

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,15 +1,9 @@
 global.chai = require('chai');
 global.chaiHttp = require('chai-http');
 global.sinon = require('sinon');
+global.mongoose = require('mongoose');
+global.mockgoose = require('mockgoose');
 process.env.NODE_ENV = 'dev';
 process.env.MONGO_DB_URI = 'localhost';
 global.expect = chai.expect;
 chai.use(chaiHttp);
-
-const mongoose = require('mongoose');
-const mockgoose = require('mockgoose');
-
-mockgoose(mongoose).then(() => {
-  global.server = require('../index');
-  done();
-});
