01bafbf6d6e998d1d2b9d53f9857f7180e724c4f
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-02-13 14:29:59 -0400

    test for cover findById

--- test/functional/user_crud.js
+++ test/functional/user_crud.js
@@ -1,4 +1,7 @@
 const User1 = require('../../model/user/user-schema');
+const Controller = require('../../lib/controller');
+var user_bd = {};
+
 mockgoose(mongoose).then(() => {
   global.server = require('../../index');
   done();
@@ -23,20 +26,20 @@ it('should create a new user', (done) => {
   });
 });
 
-  it('should not update a user', (done) => {
-    const User = new User1();
-    const Uid = '587298a376d5036c68b6ef12';
-    chai.request(server)
-        .put('/user/' + Uid)
-        .set({ origin: process.env.AllowUrl })
-        .send({ userType: 'coolGuy' })
-        .end((err, res) => {
-          expect(res).to.have.status(500);
-          expect(res.error);
-          done();
-        });
-    // done();
-  });
+it('should not update a user', (done) => {
+  const User = new User1();
+  const Uid = '587298a376d5036c68b6ef12';
+  chai.request(server)
+      .put('/user/' + Uid)
+      .set({ origin: process.env.AllowUrl })
+      .send({ userType: 'coolGuy' })
+      .end((err, res) => {
+        expect(res).to.have.status(500);
+        expect(res.error);
+        done();
+      });
+  // done();
+});
 
 it('should modify a user', (done) => {
   const User = new User1();
@@ -53,5 +56,30 @@ it('should modify a user', (done) => {
           // TODO: Write a GET request to verify that user's name has been changed
           done();
         });
+});
 
+it('should find a user by id', (done) => {
+  const User = new User1();
+  User.name = 'foo';
+  User.email = 'foo3@bar.com';
+  User.save();
+  chai.request(server)
+    .get('/user/' + User._id)
+    .set({origin: process.env.AllowUrl})
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      done();
+    });
+}); 
+
+it('should NOT find a user by id', (done) => {
+  const User = new User1();
+  var id = "587298a376d5036c68b6ef12";
+  chai.request(server)
+    .get('/user/' + id)
+    .set({origin: process.env.AllowUrl})
+    .end((err, res) => {
+      expect(res).to.have.status(404);
+      done();
+  });
 });
